6a4b1a8aae67e73703a58247a24fca69029425d0
ThetaSygma <aymeric.d87@gmail.com>
2022-03-19 19:02:40 +0100

    routeur

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ClassView from '../views/ClassView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/classroom',
+    name: 'ShowClassroom',
+    component: ClassView
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
