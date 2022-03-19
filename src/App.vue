2a689c6dc925035ec075ea3fada6ecc8a1bd01ed
ThetaSygma <aymeric.d87@gmail.com>
2022-03-19 17:35:19 +0100

    alexis

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello Alexis"/>
     <font-awesome-icon icon="fa-solid fa-user-secret" />
   </div>
 </template>
@@ -16,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
